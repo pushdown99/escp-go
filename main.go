a4ebb5b2404837019bc94e62f1aadae0ce43192e
unknown <haeyun@gmail.com>
2020-12-13 17:20:30 +0900

    make it better

--- main.go
+++ main.go
@@ -1,91 +1,70 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"io"
-	"log"
+	"github.com/gorilla/mux"
+    "html/template"
 	"net/http"
-	"os"
-
-	"github.com/gernest/utron"
-	"github.com/gernest/utron/controller"
-	"github.com/gernest/utron/router"
+	"log"
+	//"fmt"
+	"encoding/json"
+	"io/ioutil"
 )
 
-type indexView struct {
-	t *template.Template
+//////////////////////////////////////////////////////////////
+
+type Setting struct {
+	Name  		string 
+	Register	string
+	Address		string
+	POS			string
+	Printer		string
+    Using 		bool
 }
 
-func newIndexView(file string) (*indexView, error) {
-	t, err := template.ParseFiles(file)
-	if err != nil {
-		return nil, err
-	}
-	return &indexView{t: t}, nil
+var data = Setting {
+	Name: 		"Input name",
+	Register: 	"Input business register number",
+	Address: 	"Input address",
+	POS: 		"COM1",
+	Printer: 	"COM2",
+	Using: 		true,
+};
+
+//////////////////////////////////////////////////////////////
+
+func Index (w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, data)
 }
 
-func (v indexView) Render(out io.Writer, name string, data interface{}) error {
-	return v.t.ExecuteTemplate(out, name, data)
+func Get (w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name := params["name"]
+	w.Write([]byte("Hello " + name))
 }
 
-type Upload struct {
-	controller.BaseController
-	Routes []string
-}
-
-func (u *Upload) Index() {
-	u.Ctx.Template = "index.html"
-}
-
-//Save saves the uploaded file
-func (u *Upload) Save() {
-	r := u.Ctx.Request()
-	err := r.ParseMultipartForm(15485760)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	f, h, err := r.FormFile("upload")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer f.Close()
-	o, err := os.OpenFile(h.Filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer o.Close()
-	io.Copy(o, f)
-	fmt.Println("OK")
-}
-
-func NewUpload() controller.Controller {
-	return &Upload{
-		Routes: []string{
-			"get;/;Index",
-			"post;/upload;Save",
-			"get;/delete/{id};Delete",
-		},
-	}
+func Upload (w http.ResponseWriter, r *http.Request) {
+	//params := mux.Vars(r)
+	//name := params["name"]
+	data.Name 		= r.FormValue("name")
+	data.Register 	= r.FormValue("register")
+	data.Address 	= r.FormValue("address")
+	data.POS 		= r.FormValue("pos")
+	data.Printer 	= r.FormValue("printer")
+	data.Using      = r.FormValue("using") == "on"
+	file, _ := json.MarshalIndent(data, "", " ")
+	_ = ioutil.WriteFile("test.json", file, 0644)
+	http.Redirect(w, r, "http://"+r.Host+"/", http.StatusSeeOther)
 }
 
 func main() {
-	app := utron.NewApp()
-	v, err := newIndexView("index.html")
-	if err != nil {
-		app.Log.Errors(err)
-		return
-	}
-	app.View = v
-	app.Router.Options = &router.Options{
-		View: v,
-	}
-	app.Router.Add(NewUpload)
-	port := ":8090"
-	app.Log.Info("staring server on port", port)
-	log.Fatal(http.ListenAndServe(port, app))
+	r := mux.NewRouter ()
+    r.HandleFunc("/", Index)
+    r.HandleFunc("/upload", Upload).Methods("POST")
+	//rtr.HandleFunc("/user/{name:[a-z]+}/profile", profile).Methods("GET")
 
+	http.Handle("/", r)
+	port := ":80"
+	log.Println("staring server on port", port)
+	log.Fatal(http.ListenAndServe(port, nil)) 
 }
